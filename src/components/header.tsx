b19b81da6a1bb1d36b098dc189fbfd666c66b56f
aamct2 <aamct2@gmail.com>
2020-05-31 12:46:00 +0100

    Remove styling and more boilerplate

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,29 +2,10 @@ import { Link } from 'gatsby'
 import React from 'react'
 
 const Header: React.FC<{ siteTitle: string }> = ({ siteTitle }) => (
-  <header
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <header>
+    <div>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,10 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to &quot;Using TypeScript&quot;</Link>
+    <h1>Welcome to Aaron's cooking blog</h1>
+    <p>
+      This site contains a record of some of my cooking and baking projects.
+    </p>
   </Layout>
 )
 
