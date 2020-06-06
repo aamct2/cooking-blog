c2c362c24aeca472199b93893e5f951f8822eedf
aamct2 <aamct2@gmail.com>
2020-06-06 19:18:44 +0100

    Style the page header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import './Header.scss'
 import { Link } from 'gatsby'
 import React from 'react'
 
@@ -6,12 +7,10 @@ import React from 'react'
  * @param siteTitle Title of the website
  */
 const Header: React.FC<{ siteTitle: string }> = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
-    </div>
+  <header className="Header">
+    <h1 style={{ margin: 0 }}>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
   </header>
 )
 
