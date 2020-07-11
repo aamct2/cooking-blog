db98231e02072a107a7804ad692e9fcc833f7019
aamct2 <aamct2@users.noreply.github.com>
2020-07-11 06:55:07 +0000

    deploy: 2c1cb9a435af830814b84e11b035ce649e5f3e00

--- component---src-pages-404-tsx-444fa421f6982d4c97ce.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("soUV"),u=n("q1tI"),l=n.n(u),o=n("1Yd/");t.default=function(){return l.a.createElement(a.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-444fa421f6982d4c97ce.js.map
